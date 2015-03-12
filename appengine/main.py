d4a4a77c4b667c020410c4039f1d31989a28a62e
Brian Reber <reber.brian@gmail.com>
2015-03-11 21:02:39 -0500

    Support away and set temp

--- appengine/main.py
+++ appengine/main.py
@@ -2,6 +2,7 @@ from google.appengine.api import urlfetch
 from keys import keys
 import json
 import logging
+import urllib2
 import webapp2
 
 class RedirectAuth(webapp2.RequestHandler):
@@ -10,6 +11,7 @@ class RedirectAuth(webapp2.RequestHandler):
 
 class AccessTokenHandler(webapp2.RequestHandler):
     def get(self, auth_code):
+        # See https://developer.nest.com/documentation/cloud/rest-quick-guide
         url = 'https://api.home.nest.com/oauth2/access_token?client_id=%s&code=%s&client_secret=%s&grant_type=authorization_code'
         result = urlfetch.fetch(url=url % (keys.NEST_CLIENT_ID, auth_code, keys.NEST_CLIENT_SECRET),
             method=urlfetch.POST,
@@ -18,57 +20,115 @@ class AccessTokenHandler(webapp2.RequestHandler):
         logging.warn(result.status_code)
         logging.warn(result.content)
 
-        response_data = json.loads(result.content)
-
         token = {}
+        if result.status_code == 200:
+            response_data = json.loads(result.content)
 
-        if 'error' in response_data:
-            token['expires_in'] = 0
-            token['access_token'] = response_data['error']
-        else:
-            token['expires_in'] = response_data['expires_in']
-            token['access_token'] = response_data['access_token']
-
+            if not 'error' in response_data:
+                token['expires_in'] = response_data['expires_in']
+                token['access_token'] = response_data['access_token']
 
         self.response.headerlist = [('Content-type', 'application/json')]
         self.response.out.write(json.dumps(token))
 
 class NestStatusHandler(webapp2.RequestHandler):
     def get(self, access_token):
+        # See https://developer.nest.com/documentation/api-reference
         url = "https://developer-api.nest.com/?auth=%s" % access_token
         result = urlfetch.fetch(url)
 
         logging.warn(result.status_code)
         logging.warn(result.content)
 
-        # Parse the result as json
-        response_data = json.loads(result.content)
-
-        # Get a list of all the structures
-        structures_json = response_data['structures']
-        structure_names = [p for p in structures_json]
-
-        # Just use the first structure
-        structure = structures_json[structure_names[0]]
-
-        # Find all the devices
-        devices = response_data['devices']
-
-        # Get the thermostat corresponding to the first thermostat
-        # in the structures list
-        thermostat = devices['thermostats'][structure['thermostats'][0]]
-
         status = {}
-        status['target_temp'] = thermostat['target_temperature_f']
-        status['current_temp'] = thermostat['ambient_temperature_f']
-        status['hvac_mode'] = thermostat['hvac_mode']
-        status['away_status'] = not 'home' == structure['away']
+        if result.status_code == 200:
+            # Parse the result as json
+            response_data = json.loads(result.content)
+
+            # Get a list of all the structures
+            structures_json = response_data['structures']
+            structure_names = [p for p in structures_json]
+
+            # Just use the first structure
+            structure = structures_json[structure_names[0]]
+
+            # Find all the devices
+            devices = response_data['devices']
+
+            # Get the thermostat corresponding to the first thermostat
+            # in the structures list
+            thermostat = devices['thermostats'][structure['thermostats'][0]]
+
+            status['thermostat'] = structure['thermostats'][0]
+            status['target_temp'] = thermostat['target_temperature_f']
+            status['current_temp'] = thermostat['ambient_temperature_f']
+            status['hvac_mode'] = thermostat['hvac_mode']
+            status['away_status'] = not 'home' == structure['away']
 
         self.response.headerlist = [('Content-type', 'application/json')]
+        self.response.out.write(json.dumps(status))
+
+
+class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
+    def redirect_request(self, req, fp, code, msg, hdrs, newurl):
+        # see https://github.com/python-git/python/blob/master/Lib/urllib2.py
+        logging.warn('redirect_request: %s' % newurl)
+        newurl = newurl.replace(' ', '%20')
+        req =  urllib2.Request(newurl,
+                               data=req.data,
+                               headers=req.headers,
+                               origin_req_host=req.get_origin_req_host(),
+                               unverifiable=True)
+        req.get_method = lambda: 'PUT'
+
+        return req
+
+class NestTargetTemperatureSetHandler(webapp2.RequestHandler):
+    def get(self, access_token, thermostat, target_temp):
+        # See https://developer.nest.com/documentation/api-reference
+        url = "https://developer-api.nest.com/devices/thermostats/%s/target_temperature_f?auth=%s" % (thermostat, access_token)
+        request = urllib2.Request(url, data='%d' % int(target_temp))
+        request.add_header('Content-Type', 'application/json')
+        request.get_method = lambda: 'PUT'
+
+        opener = urllib2.build_opener(SmartRedirectHandler(), urllib2.HTTPCookieProcessor()) #, urllib2.HTTPSHandler(debuglevel=1)
+        result = opener.open(request)
+
+        logging.warn(result.getcode())
+
+        status = {
+            'status': result.getcode(),
+            'req': 'temp',
+            'val': target_temp
+        }
+
+        self.response.out.write(json.dumps(status))
+
+class NestAwaySetHandler(webapp2.RequestHandler):
+    def get(self, access_token, structure_id, away):
+        # See https://developer.nest.com/documentation/api-reference
+        url = "https://developer-api.nest.com/structures/%s/away?auth=%s" % (structure_id, access_token)
+        request = urllib2.Request(url, data='%s' % away)
+        request.add_header('Content-Type', 'application/json')
+        request.get_method = lambda: 'PUT'
+
+        opener = urllib2.build_opener(SmartRedirectHandler(), urllib2.HTTPCookieProcessor()) #, urllib2.HTTPSHandler(debuglevel=1)
+        result = opener.open(request)
+
+        logging.warn(result.getcode())
+
+        status = {
+            'status': result.getcode(),
+            'req': 'away',
+            'val': away
+        }
+
         self.response.out.write(json.dumps(status))
 
 app = webapp2.WSGIApplication([
     ('/', RedirectAuth),
     ('/api/accesstoken/(.+?)', AccessTokenHandler),
     ('/api/status/(.+?)', NestStatusHandler),
+    ('/api/target/set/([^/]+?)/([^/]+?)/(.+?)', NestTargetTemperatureSetHandler),
+    ('/api/away/set/([^/]+?)/([^/]+?)/(.+?)', NestAwaySetHandler),
 ], debug=False)
--- iq/source/NestApi.mc
+++ iq/source/NestApi.mc
@@ -6,6 +6,8 @@ using Toybox.WatchUi as Ui;
 enum
 {
     ACCESS_TOKEN,
+    THERMOSTAT,
+    STRUCTURE,
     TARGET_TEMP,
     CURRENT_TEMP,
     IS_CURRENTLY_AWAY,
@@ -14,56 +16,99 @@ enum
 
 class KeyboardListener extends Ui.TextPickerDelegate {
     function onTextEntered(text, changed) {
-        authenticate(text);
+        var requests = new WebRequests();
+        requests.authenticate(text);
     }
 }
 
-function authenticate(code) {
-    var url = "http://nestiqapi.appspot.com/api/accesstoken/" + code;
-    Sys.println(url);
-    Comm.makeJsonRequest(url, {}, {}, method(:authenticateResponseCallback));
-}
-
-function authenticateResponseCallback(responseCode, data) {
-    Sys.println("Response: (" + responseCode + ") " + data);
-
-    if (responseCode == 200) {
-        var app = App.getApp();
-
-        // If we have an access token, we can go on with our requests
-        if (data.hasKey("access_token")) {
-            app.setProperty(ACCESS_TOKEN, data["access_token"]);
-
-            NestApi.fetchUpdates();
-        }
-
-        Ui.requestUpdate();
+const BASE_URL = "http://nestiqapi.appspot.com";
+class WebRequests {
+    function authenticate(code) {
+        var url = BASE_URL + "/api/accesstoken/" + code;
+        Comm.makeJsonRequest(url, {}, {}, method(:authenticateResponseCallback));
     }
-}
 
-function refreshData() {
-    var app = App.getApp();
-    var token = app.getProperty(ACCESS_TOKEN);
-    var url = "http://nestiqapi.appspot.com/api/status/" + token;
-    Sys.println(url);
-    Comm.makeJsonRequest(url, {}, {}, method(:refreshDataResponseCallback));
-}
+    function authenticateResponseCallback(responseCode, data) {
+        Sys.println("Response: (" + responseCode + ") " + data);
 
-function refreshDataResponseCallback(responseCode, data) {
-    Sys.println("Response: (" + responseCode + ") " + data);
+        if (responseCode == 200) {
+            var app = App.getApp();
 
-    if (responseCode == 200) {
-        var app = App.getApp();
+            // If we have an access token, we can go on with our requests
+            if (data.hasKey("access_token")) {
+                app.setProperty(ACCESS_TOKEN, data["access_token"]);
 
-        // If we have an hvac_mode, we are responding to an update
-        if (data.hasKey("hvac_mode")) {
-            app.setProperty(TARGET_TEMP, data.get("target_temp"));
-            app.setProperty(CURRENT_TEMP, data.get("current_temp"));
-            app.setProperty(IS_CURRENTLY_AWAY, data.get("away_status"));
-            app.setProperty(HVAC_MODE, data.get("hvac_mode"));
+                NestApi.fetchUpdates();
+            }
+
+            Ui.requestUpdate();
         }
+    }
 
-        Ui.requestUpdate();
+    function refreshData() {
+        var app = App.getApp();
+        var token = app.getProperty(ACCESS_TOKEN);
+        var url = BASE_URL + "/api/status/" + token;
+        Sys.println(url);
+        Comm.makeJsonRequest(url, {}, {}, method(:refreshDataResponseCallback));
+    }
+
+    function refreshDataResponseCallback(responseCode, data) {
+        Sys.println("Refresh Response: (" + responseCode + ") " + data);
+
+        if (responseCode == 200) {
+            var app = App.getApp();
+
+            // If we have an hvac_mode, we are responding to an update
+            if (data.hasKey("thermostat")) {
+                app.setProperty(THERMOSTAT, data.get("thermostat"));
+                app.setProperty(STRUCTURE, data.get("structure"));
+                app.setProperty(TARGET_TEMP, data.get("target_temp"));
+                app.setProperty(CURRENT_TEMP, data.get("current_temp"));
+                app.setProperty(IS_CURRENTLY_AWAY, data.get("away_status"));
+                app.setProperty(HVAC_MODE, data.get("hvac_mode"));
+            }
+
+            Ui.requestUpdate();
+        }
+    }
+
+    function setTargetTemperature(target) {
+        var app = App.getApp();
+        var token = app.getProperty(ACCESS_TOKEN);
+        var thermostat = app.getProperty(THERMOSTAT);
+        var url = BASE_URL + "/api/target/set/" + token + "/" + thermostat + "/" + target;
+        Sys.println(url);
+        Comm.makeJsonRequest(url, {}, {}, method(:updateDataResponseCallback));
+    }
+
+    function setAwayStatus(status) {
+        var awayStatus = "away";
+        if (!status) {
+            awayStatus = "home";
+        }
+        var url = BASE_URL + "/api/away/set/" + awayStatus;
+        Comm.makeJsonRequest(url, {}, {}, method(:updateDataResponseCallback));
+    }
+
+    function updateDataResponseCallback(responseCode, data) {
+        Sys.println("Update Response: (" + responseCode + ") " + data);
+
+        if ((responseCode == 200) &&
+            data.hasKey("status") &&
+            (data.get("status") == 200))
+        {
+            var app = App.getApp();
+            var initialReq = data.get("req");
+
+            if ("temp".equals(initialReq)) {
+                app.setProperty(TARGET_TEMP, data.get("val"));
+            } else if ("away".equals(initialReq)) {
+                app.setProperty(IS_CURRENTLY_AWAY, data.get("val"));
+            }
+
+            Ui.requestUpdate();
+        }
     }
 }
 
@@ -73,12 +118,12 @@ class NestApi {
     static function isAuthenticated() {
         var app = App.getApp();
         var token = app.getProperty(ACCESS_TOKEN);
-        Sys.println("" + token);
         return (token != null);
     }
 
     static function fetchUpdates() {
-        refreshData();
+        var requests = new WebRequests();
+        requests.refreshData();
     }
 
     static function getTargetTemp() {
@@ -107,13 +152,13 @@ class NestApi {
 
     static function setTargetTemperature(target) {
         if ((target > 50) && (target < 90)) {
-            var url = "https://nestiqapi.appspot.com/_ah/api/nestiq/v1/nest/targettemperature/set/" + target;
-            Comm.makeJsonRequest(url, {}, {}, responseCallback);
+            var requests = new WebRequests();
+            requests.setTargetTemperature(target);
         }
     }
 
     static function setAwayStatus(status) {
-        var url = "https://nestiqapi.appspot.com/_ah/api/nestiq/v1/nest/away/set/" + status;
-        Comm.makeJsonRequest(url, {}, {}, responseCallback);
+        var requests = new WebRequests();
+        requests.setAwayStatus(status);
     }
 }
