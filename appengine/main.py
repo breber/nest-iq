b56ca4a4a20cdf100857f5303678acca5df2b618
Brian Reber <reber.brian@gmail.com>
2015-03-11 21:04:41 -0500

    Add documentation links

--- appengine/main.py
+++ appengine/main.py
@@ -5,6 +5,9 @@ import logging
 import urllib2
 import webapp2
 
+# https://developer.nest.com/documentation/cloud/api-overview/
+# https://developer.nest.com/documentation/cloud/rest-guide
+
 class RedirectAuth(webapp2.RequestHandler):
     def get(self):
         self.redirect("https://home.nest.com/login/oauth2?client_id=%s&state=STATE" % keys.NEST_CLIENT_ID)
